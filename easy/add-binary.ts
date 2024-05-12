cf9be0efff6e65c63d5ca0b4a885a065f2573868
JiMarchel <jmarchel100@gmail.com>
2024-05-12 11:12:24 +0700

    add answer

--- easy/add-binary.ts
+++ easy/add-binary.ts
@@ -1,3 +1,6 @@
 function addBinary(a: string, b: string): string {
   return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
 }
+
+console.log(addBinary("11", "1"));
+// https://leetcode.com/problems/add-binary/
---
+++ easy/single-number.ts
@@ -0,0 +1,15 @@
+function singleNumber(nums: number[]): number {
+  const seen = new Set();
+
+  for (const num of nums) {
+    if (seen.has(num)) {
+      seen.delete(num);
+    } else {
+      seen.add(num);
+    }
+  }
+
+  return seen.values().next().value;
+}
+
+console.log(singleNumber([2, 2, 1, 3, 3, 3, 3, 3]));
