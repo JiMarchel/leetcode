40a944a64db689766bc69217593ffb26ae2b912b
JiMarchel <jmarchel100@gmail.com>
2024-06-06 10:42:42 +0700

    add answer

---
+++ easy/replace-elements-with-greatest-element-on-right-side.ts
@@ -0,0 +1,16 @@
+function replaceElements(arr: number[]): number[] {
+  let max = arr[arr.length - 1];
+  arr[arr.length - 1] = -1;
+
+  for (let i = arr.length - 2; i >= 0; i--) {
+    let curr = arr[i];
+    arr[i] = max;
+    if (curr > max) max = curr;
+  }
+
+  return arr;
+}
+
+console.log(replaceElements([17, 18, 5, 4, 6, 1]));
+
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
