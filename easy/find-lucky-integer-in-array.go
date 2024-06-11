ab25ab492ef02a1d8f78b254d0464ac5363aec52
JiMarchel <jmarchel100@gmail.com>
2024-06-11 10:46:24 +0700

    add answer

---
+++ easy/find-lucky-integer-in-array.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func findLucky(arr []int) int {
+	freqMap := make(map[int]int)
+
+	for _, num := range arr {
+		freqMap[num]++
+	}
+
+	lucky := -1
+	for num, freq := range freqMap {
+		if num == freq && num > lucky {
+			lucky = num
+		}
+	}
+
+	return lucky
+}
+
+func main() {
+	arr := []int{2, 2, 3, 4}
+	fmt.Println(findLucky(arr))
+}
