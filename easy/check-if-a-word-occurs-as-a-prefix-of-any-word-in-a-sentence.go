9e461f3fc50970f71d9b29c0c0f7a987efa56a25
JiMarchel <jmarchel100@gmail.com>
2024-06-12 12:41:16 +0700

    add answer

---
+++ easy/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isPrefixOfWord(sentence string, searchWord string) int {
+	words := strings.Split(sentence, " ")
+
+	for i, v := range words {
+		if strings.HasPrefix(v, searchWord) {
+			return i + 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	sentence := "i love eating burger"
+	searchWord := "burg"
+	fmt.Println(isPrefixOfWord(sentence, searchWord)) // Output: 4
+}
