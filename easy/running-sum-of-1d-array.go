a12fcddb63b0a1c1911f44e40515e0962632c0d9
JiMarchel <jmarchel100@gmail.com>
2024-06-12 16:33:17 +0700

    a

--- easy/running-sum-of-1d-array.go
+++ easy/running-sum-of-1d-array.go
@@ -1,15 +1,12 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func runningSum(nums []int) []int {
-	// Buat slice baru dengan panjang yang sama dengan nums untuk menampung hasil running sum
 	sums := make([]int, len(nums))
 
-	// Inisialisasi elemen pertama
 	sums[0] = nums[0]
 
-	// Loop melalui elemen yang tersisa
 	for i := 1; i < len(nums); i++ {
 		sums[i] = sums[i-1] + nums[i]
 	}
@@ -17,9 +14,9 @@ func runningSum(nums []int) []int {
 	return sums
 }
 
-func main() {
-	nums := []int{1, 2, 3, 4}
-	fmt.Println(runningSum(nums))
-}
+// func main() {
+// 	nums := []int{1, 2, 3, 4}
+// 	fmt.Println(runningSum(nums))
+// }
 
 // https://leetcode.com/problems/running-sum-of-1d-array/
