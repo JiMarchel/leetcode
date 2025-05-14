ad609dc9cea89fe29deafdd572b404d488e6a5c8
JiMarchel <jmarchel100@gmail.com>
2025-05-14 17:22:28 +0700

    add link

--- easy/add-two-promise.ts
+++ easy/add-two-promise.ts
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/add-two-promises/description/?envType=study-plan-v2&envId=30-days-of-javascript
 type P = Promise<number>
 
 async function addTwoPromises(promise1: P, promise2: P): P {
